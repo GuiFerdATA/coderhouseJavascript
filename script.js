8a2e75519c90f5a2e66749b9357946dc411e99c9
Guille <guille.fer79@hotmail.com>
2023-12-05 20:24:34 -0300

    asd

--- script.js
+++ script.js
@@ -1,26 +1,4 @@
-/*
-const palabraSecreta = "javascript";
-let intentos = 3;
 
-alert("¡Bienvenido al juego de adivinanza! Adivina la palabra secreta.");
-
-while (intentos > 0) {
-  const respuesta = prompt("Ingresa una palabra:").toLowerCase();
-
-  if (respuesta === palabraSecreta) {
-    alert("¡Respuesta correcta! Has ganado el juego.");
-    break;
-  } else {
-    intentos--;
-    alert(`Respuesta incorrecta. Te quedan ${intentos} intentos.`);
-  }
-}
-
-if (intentos === 0) {
-  alert(`Se te han agotado los intentos. La palabra secreta era "${palabraSecreta}".`);
-}
-
-/* 
 
 
 
@@ -55,4 +33,4 @@ jugarAdivinanza("javascript", 3);
 
 
 
-*/
+
