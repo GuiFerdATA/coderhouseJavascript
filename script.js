21e1fa0f121e85e3a6e11224eff06dc0ff8b280d
Guille <guille.fer79@hotmail.com>
2023-12-05 20:05:55 -0300

    subimos pre-entrega

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/*
 const palabraSecreta = "javascript";
 let intentos = 3;
 
@@ -18,3 +19,40 @@ while (intentos > 0) {
 if (intentos === 0) {
   alert(`Se te han agotado los intentos. La palabra secreta era "${palabraSecreta}".`);
 }
+
+/* 
+
+
+
+
+//Creamos una funcion para nuestro juego (le pasamos dos parametros)
+function jugarAdivinanza(palabraSecreta, intentosMaximos) {
+  let intentos = intentosMaximos; // emparejamos intentos con intentosMaximos
+
+  //Mediante un alert, le damos la bienvenida a nuestro usuario
+  alert("¡Bienvenido al juego de adivinanza! Adivina la palabra secreta.");
+
+  // Usamos el clico while para estructurar nuestros intentos/juego
+  while (intentos > 0) { 
+    const respuesta = prompt("Ingresa una palabra:").toLowerCase();//Le pedimos al usuario que ingrese un palabra mediante prompt y convertimos el texto a minusculas.
+
+    if (respuesta === palabraSecreta) { //ultilizamos un condicional, en este caso if/else
+      alert("¡Respuesta correcta! Has ganado el juego.");
+      return; //Si la respuesta es acertada antes de terminar los intentos, terminamos el clico con nuestro return
+    } else {  // De lo conrtrario restamos los intentos y mostramos mediante un alert info de la cantidad de intentos disponibles a nuestro usuario.
+      intentos--;
+      alert(`Respuesta incorrecta. Te quedan ${intentos} intentos.`);
+    }
+  }
+
+  if (intentos === 0) { // una vez fuera de nuestro ciclo(cuando el usuario gasto sus 3 oportunidades damos por terminado el juego.)
+    alert(`Se te han agotado los intentos. La palabra secreta era "${palabraSecreta}".`);
+  }
+}
+
+// Llamamos a nuestra funcion, declarando su valores (javascript(como palabra clave) y 3 (como numero de intentos)).
+jugarAdivinanza("javascript", 3);
+
+
+
+*/
